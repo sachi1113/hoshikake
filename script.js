d19eee264901b085e968e2467cada42efe9b1504
sachi_design13 <89501161+sachi1113@users.noreply.github.com>
2021-08-28 09:41:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ $(function(){
 	$(function(){
     $(window).on('load',function(){
   $("#splash").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
-  $("#splash_logo").delay(1500).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
+  $("#splash_logo").delay(1300).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
 });
 });
 
